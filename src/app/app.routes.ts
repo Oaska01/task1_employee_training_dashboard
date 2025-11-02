3d0b546426d934323e3457d30332824a5953106e
Oaska01 <osmanaskar195@gmail.com>
2025-11-02 12:32:31 +0200

    setting routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Navbar } from './components/navbar/navbar';
+import { EmployeeList } from './components/employee-list/employee-list';
+import { Dashboard } from './components/dashboard/dashboard';
+import { TrainingForm } from './components/training-form/training-form';
+import { TrainingList } from './components/training-list/training-list';
+
+export const routes: Routes = [
+
+    {path : "dashboard", component : Dashboard},
+    {path : "employee-list", component : EmployeeList},
+    {path : "training-form", component : TrainingForm},
+    {path : "training-list", component : TrainingList},
+
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component, signal } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterLink } from '@angular/router';
+
+import { Navbar } from './components/navbar/navbar';
+import { EmployeeList } from './components/employee-list/employee-list';
+import { Dashboard } from './components/dashboard/dashboard';
+import { TrainingForm } from './components/training-form/training-form';
+import { TrainingList } from './components/training-list/training-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, EmployeeList, Dashboard, TrainingForm, TrainingList,
+    RouterModule, RouterLink
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
