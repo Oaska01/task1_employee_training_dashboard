f11d8a84a8161a439359c9ada669eca4bca08b9a
Oaska01 <osmanaskar195@gmail.com>
2025-11-02 12:43:58 +0200

    modifying routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,15 @@ import { EmployeeList } from './components/employee-list/employee-list';
 import { Dashboard } from './components/dashboard/dashboard';
 import { TrainingForm } from './components/training-form/training-form';
 import { TrainingList } from './components/training-list/training-list';
+import { NotFound } from './components/not-found/not-found';
 
 export const routes: Routes = [
 
-    {path : "dashboard", component : Dashboard},
-    {path : "employee-list", component : EmployeeList},
-    {path : "training-form", component : TrainingForm},
-    {path : "training-list", component : TrainingList},
+    { path : "", redirectTo : "dashboard", pathMatch : "full" },
+    { path : "dashboard", component : Dashboard },
+    { path : "employee-list", component : EmployeeList },
+    { path : "training-form", component : TrainingForm },
+    { path : "training-list", component : TrainingList },
+    { path : "**", component : NotFound}
 
 ];
