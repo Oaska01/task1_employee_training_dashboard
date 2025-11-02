168c9820b5df37441207bcb50665fceb3c11044a
Oaska01 <osmanaskar195@gmail.com>
2025-11-02 13:31:34 +0200

    creating training from

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+import { TrainingForm } from "../training-form/training-form";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Navbar, TrainingForm],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
--- src/app/components/training-list/training-list.ts
+++ src/app/components/training-list/training-list.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { TrainingForm } from '../training-form/training-form';
 
 @Component({
   selector: 'app-training-list',
-  imports: [],
+  imports: [RouterLink,],
   templateUrl: './training-list.html',
   styleUrl: './training-list.css',
 })
