5a479dc64802ba0108796c8aa4246f2f697dd3dc
Oaska01 <osmanaskar195@gmail.com>
2025-11-02 12:19:17 +0200

    creating components and seting a navbar from bootstrap

---
+++ src/app/components/training-list/training-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-training-list',
+  imports: [],
+  templateUrl: './training-list.html',
+  styleUrl: './training-list.css',
+})
+export class TrainingList {
+
+}
